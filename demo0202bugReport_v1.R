4574e1a10f0613aaf1f90967d80b03010a2ed394
HCH1 <theone_730801@hotmail.com>
2017-02-06 10:49:38 +0800

    Update demo0202bugReport_v1.R

--- demo0202bugReport_v1.R
+++ demo0202bugReport_v1.R
@@ -14,39 +14,72 @@ write.csv(x = sfbug, file = "sfbug.csv")
 #Design Manual	
 #22FDX 22FDSOI 22SOI
 sfbug_orig <- sfbug[ which( sfbug[2]=="22FDX" | sfbug[2]=="22FDSOI" | sfbug[2]=="22SOI"), ]
-sfbug1 <- sfbug[ which( sfbug[2]=="22FDX" | sfbug[2]=="22FDSOI" | sfbug[2]=="22SOI"), ]
-sfbug1 <- sfbug1[sfbug1[5]=="Design Manual", ]
-#to filter date region
-sfbug1 <- sfbug1[ sfbug1[13] >= "2016-11-10", ]
-sfbug1 <- sfbug1[ sfbug1[13] <= "2017-01-31", ]
+bug_all <- sfbug[ which( sfbug[2]=="22FDX" | sfbug[2]=="22FDSOI" | sfbug[2]=="22SOI"), ]
+bug_all <- bug_all[bug_all[5]=="Design Manual", ]
+#to filter date region 11/10-1/31
+bug_all <- bug_all[ bug_all[13] >= "2016-11-10", ]
+bug_all <- bug_all[ bug_all[13] <= "2017-01-31", ]
 write.csv(x = sfbug_orig, file = "sfbug_orig.csv")
-write.csv(x = sfbug1, file = "sfbug1.csv")
-
+write.csv(x = bug_all, file = "bug_all.csv")
+#Resolved
+#Verified
+#Closed
+close_all <- bug_all[ which( bug_all[4]=="Resolved" | bug_all[4]=="Verified" | bug_all[4]=="Closed") , ]
+open_all <- bug_all[ which( bug_all[4]!="Resolved" & bug_all[4]!="Verified" & bug_all[4]!="Closed") , ]
+write.csv(x = close_all, file = "close_all.csv")
+write.csv(x = open_all, file = "open_all.csv")
+open_DM <- open_all[ which( open_all[11]=="Error (Others)" | open_all[11]=="Typo Implementation" | open_all[11]=="Typo Proposal" | open_all[11]=="Inadequate Proposal" | open_all[11]=="Need IAD"  | open_all[11]=="Neglected Related Content" | open_all[11]=="Scope changed"), ]
+write.csv(x = open_DM, file = "open_DM.csv")
 #(blank)
 #Work Request
 #Enhancement Request
 #Question (No Update Required)
+#Question
 #Invalid Bug
-nonDMbug <- sfbug1[ which( sfbug1[11]=="Enhancement request" | sfbug1[11]=="Work request" | sfbug1[11]=="Invalid Bug" | sfbug1[11]=="Question (No Update Required)") , ]
-nonDMbug_withBlank <- sfbug1[ which( sfbug1[11]=="" | sfbug1[11]=="Enhancement request" | sfbug1[11]=="Work request" | sfbug1[11]=="Invalid Bug" | sfbug1[11]=="Question (No Update Required)") , ]
+open_Enhance <- open_all[open_all[11]=="Enhancement Request", ]
+open_Work <- open_all[open_all[11]=="Work Request", ]
+open_Ques <- open_all[which( open_all[11]=="Question (No Update Required)" | open_all[11]=="Question"), ]
+open_Invalid <- open_all[open_all[11]=="Invalid Bug", ]
+open_Blank <- open_all[open_all[11]=="", ]
+write.csv(x = open_Enhance, file = "open_Enhance.csv")
+write.csv(x = open_Work, file = "open_Work.csv")
+write.csv(x = open_Ques, file = "open_Ques.csv")
+write.csv(x = open_Invalid, file = "open_Invalid.csv")
+write.csv(x = open_Blank, file = "open_Blank.csv")
+
+sum_count <- rbind( dim(open_DM),dim(open_Enhance),dim(open_Work),dim(open_Ques),dim(open_Invalid),dim(open_Blank),dim(close_all) )
+rownames(sum_count) <- c("open_DM","open_Enhance","open_Work","open_Ques","open_Invalid","open_Blank","close_all")
+colnames(sum_count) <- c("bug (ea)","")
+write.csv(x = sum_count, file = "sum_count.csv")
+#Plot the chart.
+pie(sum_count[,1])
+#-------------------
+#(blank)
+#Work Request
+#Enhancement Request
+#Question (No Update Required)
+#Question
+#Invalid Bug
+#nonDMbug <- sfbug1[ which( sfbug1[11]=="Enhancement request" | sfbug1[11]=="Work request" | sfbug1[11]=="Invalid Bug" | sfbug1[11]=="Question (No Update Required)") , ]
+#nonDMbug_withBlank <- sfbug1[ which( sfbug1[11]=="" | sfbug1[11]=="Enhancement request" | sfbug1[11]=="Work request" | sfbug1[11]=="Invalid Bug" | sfbug1[11]=="Question (No Update Required)") , ]
 #Typo Proposal
 #Error (Others)
 #Inadequate Proposal
 #Typo Implementation
 #Scope changed
 #Neglected Related Content
-DMbug <- sfbug1[ which( sfbug1[11]=="Error (Others)" | sfbug1[11]=="Typo Implementation" | sfbug1[11]=="Typo Proposal" | sfbug1[11]=="Inadequate Proposal" | sfbug1[11]=="Need IAD"  | sfbug1[11]=="Neglected Related Content" | sfbug1[11]=="Scope changed") , ]
-write.csv(x = nonDMbug, file = "nonDMbug.csv")
-write.csv(x = DMbug, file = "DMbug.csv")
+#DMbug <- sfbug1[ which( sfbug1[11]=="Error (Others)" | sfbug1[11]=="Typo Implementation" | sfbug1[11]=="Typo Proposal" | sfbug1[11]=="Inadequate Proposal" | sfbug1[11]=="Need IAD"  | sfbug1[11]=="Neglected Related Content" | sfbug1[11]=="Scope changed") , ]
+#write.csv(x = nonDMbug, file = "nonDMbug.csv")
+#write.csv(x = DMbug, file = "DMbug.csv")
 #blank <- sfbug1[ sfbug1[11]=="" , ]
 
 #Resolved
 #Verified
 #Closed
-nonDMbug_close <- nonDMbug[ which( nonDMbug[4]=="Resolved" | nonDMbug[4]=="Verified" | nonDMbug[4]=="Closed") , ]
-DMbug_close <- DMbug[ which( DMbug[4]=="Resolved" | DMbug[4]=="Verified" | DMbug[4]=="Closed") , ]
-write.csv(x = nonDMbug_close, file = "nonDMbug_close.csv")
-write.csv(x = DMbug_close, file = "DMbug_close.csv")
+#nonDMbug_close <- nonDMbug[ which( nonDMbug[4]=="Resolved" | nonDMbug[4]=="Verified" | nonDMbug[4]=="Closed") , ]
+#DMbug_close <- DMbug[ which( DMbug[4]=="Resolved" | DMbug[4]=="Verified" | DMbug[4]=="Closed") , ]
+#write.csv(x = nonDMbug_close, file = "nonDMbug_close.csv")
+#write.csv(x = DMbug_close, file = "DMbug_close.csv")
 
 #New
 #Assigned
@@ -54,15 +87,15 @@ write.csv(x = DMbug_close, file = "DMbug_close.csv")
 #Committed
 #Info Required
 #New Request
-nonDMbug_open <- nonDMbug[ which( nonDMbug[4]=="New" | nonDMbug[4]=="Assigned" | nonDMbug[4]=="Accepted" | nonDMbug[4]=="Committed" | nonDMbug[4]=="Info Required" | nonDMbug[4]=="New Request") , ]
-DMbug_open <- DMbug[ which( DMbug[4]=="New" | DMbug[4]=="Assigned" | DMbug[4]=="Accepted" | DMbug[4]=="Committed" | DMbug[4]=="Info Required" | DMbug[4]=="New Request") , ]
-write.csv(x = nonDMbug_open, file = "nonDMbug_open.csv")
-write.csv(x = DMbug_open, file = "DMbug_open.csv")
+#nonDMbug_open <- nonDMbug[ which( nonDMbug[4]=="New" | nonDMbug[4]=="Assigned" | nonDMbug[4]=="Accepted" | nonDMbug[4]=="Committed" | nonDMbug[4]=="Info Required" | nonDMbug[4]=="New Request") , ]
+#DMbug_open <- DMbug[ which( DMbug[4]=="New" | DMbug[4]=="Assigned" | DMbug[4]=="Accepted" | DMbug[4]=="Committed" | DMbug[4]=="Info Required" | DMbug[4]=="New Request") , ]
+#write.csv(x = nonDMbug_open, file = "nonDMbug_open.csv")
+#write.csv(x = DMbug_open, file = "DMbug_open.csv")
 
-count <- rbind( dim(DMbug),dim(nonDMbug),dim(nonDMbug_withBlank),dim(DMbug_open),dim(nonDMbug_open),dim(DMbug_close),dim(nonDMbug_close) )
-rownames(count) <- c("DMbug","nonDMbug","nonDMbug_withBlank","DMbug_open","nonDMbug_open","DMbug_close","nonDMbug_close")
-colnames(count) <- c("bug (ea)","col (ea)")
-write.csv(x = count, file = "count.csv")
+#count <- rbind( dim(DMbug),dim(nonDMbug),dim(nonDMbug_withBlank),dim(DMbug_open),dim(nonDMbug_open),dim(DMbug_close),dim(nonDMbug_close) )
+#rownames(count) <- c("DMbug","nonDMbug","nonDMbug_withBlank","DMbug_open","nonDMbug_open","DMbug_close","nonDMbug_close")
+#colnames(count) <- c("bug (ea)","col (ea)")
+#write.csv(x = count, file = "count.csv")
 
 #DMbug_open <- sfbug1_DMbug[1] - DMbug_close[1]
 #DMbug_open <- sfbug1_DMbug[sfbug1_DMbug[1] != DMbug_close[1],]
