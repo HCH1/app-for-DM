dd7dd5ef9b6833611c41dd9c4f9d1665dd1b4ffc
HCH1 <theone_730801@hotmail.com>
2017-11-30 16:49:41 +0800

    Add files via upload

---
+++ ui_SOPcountTerms.R
@@ -0,0 +1,51 @@
+library(shiny)
+
+ui <- fluidPage(
+
+  titlePanel("Uploading File"),
+
+  sidebarLayout(
+
+    sidebarPanel(
+
+      fileInput("file1", "Pls input _internal.csv -> word counts",
+                multiple = TRUE,
+                accept = c("text/csv",
+                         "text/comma-separated-values,text/plain",
+                         ".csv")),
+
+      tags$hr(),
+
+      checkboxInput("header", "Header", TRUE),
+
+      radioButtons("sep", "Separator",
+                   choices = c(Comma = ",",
+                               Semicolon = ";",
+                               Tab = "\t"),
+                   selected = ","),
+
+      radioButtons("quote", "Quote",
+                   choices = c(None = "",
+                               "Double Quote" = '"',
+                               "Single Quote" = "'"),
+                   selected = '"'),
+
+      tags$hr(),
+
+      radioButtons("disp", "Display",
+                   choices = c(Head = "head",
+                               All = "all"),
+                   selected = "head")
+
+#      downloadButton("downloadData", "Download")
+    ),
+
+    mainPanel(
+
+      tableOutput("table1")
+
+    )
+
+  )
+)
+#runApp()
