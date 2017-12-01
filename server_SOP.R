b1b2719ebfa8acb37250b4d7ae082cda7b416699
HCH1 <theone_730801@hotmail.com>
2017-12-01 11:12:04 +0800

    Add files via upload

---
+++ server_SOP.R
@@ -0,0 +1,19 @@
+options(shiny.maxRequestSize=30*1024^2) 
+library(quanteda)
+library(tm)
+library(SnowballC)
+library(wordcloud)
+server <- function(input, output) {
+####################################################
+output$table1 <- renderTable({
+req(input$file1)
+in0 <- read.csv(input$file1$datapath, header = TRUE)
+in1 <- in0
+
+
+
+write.csv(x = tb2, row.names = FALSE, file = paste(format(Sys.time(), "%Y%m%d_%H"), "_WC_byTokens.csv", sep = "") )
+head(tb2,22)
+
+})
+}
