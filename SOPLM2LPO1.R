604802ebb5dca675753c4e4730cab4e8172ab574
HCH1 <theone_730801@hotmail.com>
2018-01-08 17:07:47 +0800

    Update SOPLM2LPO1.R

--- SOPLM2LPO1.R
+++ SOPLM2LPO1.R
@@ -11,7 +11,7 @@ dumx5 <- matrix( NA, nrow=dim(i1)[1], ncol=5 )
 for footstop "."
 dumx_fs <- matrix( ".", nrow=dim(i1)[1], ncol=1 )
 dim(dumx0)
-#create LPO_draft
+#create LPO_draft1
 mx1 <- cbind( i1[1],i1[3],i1[2],dumx5,
 #create "." or ".." at the end
 paste( i1$Description, dumx_fs, sep = "", collapse = NULL ),
@@ -20,7 +20,18 @@ dumx3,i1[4],dumx3,i1[7],i1[8],i1[9],i1[10],dumx4 )
 colnames(mx1) <- coln2[1:dim(t(mx1))[1]]
 #replace ".." -> "."
 mx1$Layer.Description <- gsub("[.]{2}", ".", mx1$Layer.Description )
+#replace start is "." -> ""
+mx1$Layer.Description <- gsub("^[.]{1}", "", mx1$Layer.Description )
 str(mx1)
 write.csv(x = mx1, row.names = FALSE, file = paste(format(Sys.time(), "%Y%m%d_%H"), "_LPO_draft1.csv", sep = "") )
+#vlookup
+library(dplyr)
+mx1_v2 <- mx1
+#vs with 22's LPO
+i2_v2 <- cbind( i2[2:7],i2[9],i2[10],i2[11],i2[13],i2[17:24] )
+dim(i2_v2)
+mx1_v2 <- left_join(mx1_v2, i2_v2, by = "Data.Layer.Name")
+str(mx1_v2)
+write.csv(x = mx1_v2, row.names = FALSE, file = paste(format(Sys.time(), "%Y%m%d_%H"), "_LPO_draft2.csv", sep = "") )
 ####################################################end
 ####################################################end
