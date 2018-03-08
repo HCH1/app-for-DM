9fe5a481acf8046ef1f974c84be82e5516edfed8
HCH1 <theone_730801@hotmail.com>
2018-03-08 17:04:36 +0800

    Update SOPcommands.py

--- SOPcommands.py
+++ SOPcommands.py
@@ -3,7 +3,32 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 df1 = pd.read_csv('//fab2crp-nas1/home19/hhung/Profile/Documents/2018py/28LPQRF-QCA_Rev1.1_0.0_DRC03D2vs28LPQRF-QCA_Rev1.1_0.0_DRC02DRCTrack2.csv')
+#extract 3 columns
+#diffdm1 <- cbind( diffdm[3], diffdm[7], diffdm[1] )
+df2 = pd.concat( [ df1.iloc[:,2],df1.iloc[:,6],df1.iloc[:,0] ] , axis=1 )
+#filter Type 
+#diffdm2 <- diffdm1[ which( diffdm1[2]=="Added"|diffdm1[2]=="Removed"|diffdm1[2]=="Modified"), ]
+df3 = df2[ df2.iloc[:,1].isin( ['Added','Removed','Modified'] ) ]
+#order Section
+#diffdm2 <- diffdm2[order(diffdm2[1]), ]
+df4 = df3.sort_index( by=['Section'] )
+#length(t(diffdm4_uni)) == 113
+#diffdm4_uni <- unique(diffdm2[1])
+df5 = pd.unique( df4.iloc[:,0] )
+
+
+#create dummy 113*1
+ans_o1 <- matrix( NA, nrow=length(t(diffdm4_uni)), ncol=1 )
+
+
+
+
+
+
+
+
 #pd.DataFrame()
+#e.g.
 dates = pd.date_range('20130101', periods=6)
 dates
 DatetimeIndex(['2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04',
@@ -34,6 +59,12 @@ df[df > 0]
 df.iat[0,1] = 0
 df.loc[:,'D'] = np.array([5] * len(df))
 pd.concat([ df.loc[:,['C']], df.loc[:,['B']] ], axis=1)
+df2[df2['E'].isin(['two','four'])] #filter
+sorted("This is a test string from Andrew".split(), key=str.lower)
+
+
+
+
 
 df = pd.DataFrame(np.random.randn(8, 4), columns=['A','B','C','D'])
 df
