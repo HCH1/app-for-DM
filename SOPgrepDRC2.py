a7863fcdb8f19e00cf6ac2e01ec652430002b301
HCH1 <theone_730801@hotmail.com>
2018-03-14 16:37:16 +0800

    Update SOPgrepDRC2.py

--- SOPgrepDRC2.py
+++ SOPgrepDRC2.py
@@ -18,7 +18,10 @@ drc2 = list( filter(lambda x: re.search(r'\w+rohibit', x), drc1) ) #grep list
 
 #http://cmdlinetips.com/2012/09/three-ways-to-write-text-to-a-file-in-python/
 #save list to txt
-o1 = open("myOutFile.txt", "w") #creat txt
+import time
+TodaysDate = time.strftime("%Y%m%d_%H")
+filename = TodaysDate + "_drc2_fil.txt"
+o1 = open(filename, "w") #creat txt
 o1.writelines(drc2) #save txt
 o1.close()
 ##or in loop
@@ -40,7 +43,8 @@ for i in range(0, 7):
 	kw1 = ['touching','over','straddling','incurring','created','space','prohibited'] #replace list
 	drc3 = [ w.replace(kw1[i], '\t') for w in drc3 ] #loop
 	print(kw1[i]) #check
-o2 = open("drc3.txt", "w") #creat txt
+fn2 = TodaysDate + "_drc2_fil_rpl.txt"
+o2 = open(fn2, "w") #creat txt
 o2.writelines( list(drc3) ) #save list txt
 o2.close()
 ###sub for regexp; replace for words
@@ -50,9 +54,21 @@ for i in range(0, 3):
 	drc4 = [ re.sub(kw2[i], "", w) for w in drc4 ] #replace
 	drc4 = [ re.sub(r'[@]', "\n", w) for w in drc4 ] #insert line
 	print(kw2[i]) #check
-o3 = open("drc4.txt", "w") #creat txt
+fn3 = TodaysDate + "_drc2_fil_rpl2.txt"
+o3 = open(fn3, "w") #creat txt
 o3.writelines( list(drc4) ) #save list txt
 o3.close()
+#trick to add word in lines
+drc5 = [ '<@head> ' + w.strip() + ' <@end>\n' for w in drc4 ] #lines head/end to add word
+fn4 = TodaysDate + "_drc2_fil_rpl2_head_end.txt"
+o_drc5 = open(fn4, "w") #creat txt
+o_drc5.writelines( list(drc5) ) #save list txt
+o_drc5.close()
+####################################################end
+drc7 = [ '<@head>'.join(w.splitlines(True)) for w in drc4 ] #lines head to add word
+o_drc7 = open("drc7.txt", "w") #creat txt
+o_drc7.writelines( list(drc7) ) #save list txt
+o_drc7.close()
 ####################################################end
 ####################################################end
 ####################################################end
