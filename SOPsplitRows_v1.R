48cd425c2529a3067369eec7672edc7b96f61aab
HCH1 <theone_730801@hotmail.com>
2018-04-16 10:23:52 +0800

    Update SOPsplitRows_v1.R

--- SOPsplitRows_v1.R
+++ SOPsplitRows_v1.R
@@ -1,5 +1,5 @@
 #library(dplyr)
-library(xlsx)
+#library(xlsx)
 i1 = read.csv("2018_LPO_draft4,lvs_lm,om,tf_ic_oa_lay,tf_ic_oa_pur,slphv,lpse - Copy6 V4 for Tom.csv", header = FALSE, stringsAsFactors=FALSE)
 dim(i1)
 mx_name <- i1[1,]
@@ -12,16 +12,16 @@ mx_ratio
 #252 to 501
 #etc
 #250(n-1)+2 to 250n+1
-for ( i in 1:mx_ratio ){
+#for ( i in 1:2 ) {
+for ( i in 1:mx_ratio ) {
 from1 <- 250*(i - 1) + 2
 end1 <- 250*i + 1
 mx_split <- rbind(mx_name, i1[from1:end1, ])
-filename1 <- paste( "28SL_Migration__Bulk_Upload_", i, "_start_", from1-1, "_end_", end1-1, ".csv", sep="" )
+filename1 <- paste( "28SL_Migration__Bulk_Upload_", i, "_start_", from1-1, "_end_", end1-1, ".xls", sep="" )
 #write.csv(x = mx_split, col.names=FALSE, file = filename1)
-write.table( mx_split, file = filename1, sep= ",", col.names=FALSE, row.names=FALSE)
+write.table( mx_split, file = filename1, sep= "\t", quote = FALSE, col.names=FALSE, row.names=FALSE)
 #write.xlsx(x = mx_split, col.names = FALSE, file = filename1)
 }
-
 ####################################################end
 ####################################################end
 ####################################################end
