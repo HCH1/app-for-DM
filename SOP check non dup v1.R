96f9362cab155224d66785a3af076e88907fbdc7
HCH1 <theone_730801@hotmail.com>
2018-05-30 13:57:33 +0800

    Update SOP check non dup v1.R

--- SOP check non dup v1.R
+++ SOP check non dup v1.R
@@ -1,17 +1,12 @@
 #input csv
 ip = read.csv("0530 demo1.csv", header = TRUE)
-ip2 <- ip
-#ip2 <- ip2[order(ip2[1]), ]
-str(ip2)
-ip4 <- table(ip2)
-head(ip4)
-str(ip4)
+ip3 <- ip
+ip4 <- table(ip3)
 ip5 <- as.data.frame(ip4)
-str(ip5)
 ip6 <- ip5[order(ip5[2], decreasing = TRUE), ]
 str(ip6)
 head(ip6)
-write.csv(x = ip6, file = paste(format(Sys.time(), "%Y%m%d_%H"), "_check non dup v1.csv", sep = "") )
+write.csv(x = ip6, file = paste(format(Sys.time(), "%Y%m%d_%H"), "_check dup v1.csv", sep = "") )
 ####################################################end
 ####################################################end
 ####################################################end
