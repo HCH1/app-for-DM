069c549cba119b9579eac5bbe128df4dd4b0ba21
HCH1 <theone_730801@hotmail.com>
2018-06-19 14:45:19 +0800

    Update and rename SOP_ch2 for 28slp v1.R to SOP_ch2 for 28g 28slp v1.R

--- SOP_ch2 for 28g 28slp v1.R
+++ SOP_ch2 for 28g 28slp v1.R
@@ -1,6 +1,7 @@
 #install.packages("zoo")
 #library(zoo)
-lpo = read.csv("LPO-000314 d1 LCN-002015.csv", header = TRUE)
+##\\fab2crp-nas1\home19\HHUNG\Profile\Documents\2018R\201805 SOPLM2LPO_for28slp_v2\201806 28slphv check again lpo vs ch2
+lpo = read.csv("LPO-000318 LCN-002051 28G.csv", header = TRUE)
 str(lpo)
 #merge column 2 6 7 17 18
 lpo2 <- cbind(lpo[2]
@@ -27,7 +28,37 @@ lpo4 <- lpo3[ which( lpo3[6]=="Common Design FEOL" | lpo3[6]=="Common Design BEO
 #for slp
 #lpo5 <- lpo4[ which( lpo4[7]=="28SLP" | lpo4[7]=="28SLP;28SLPHV"), ]
 #for hpp
-lpo5 <- lpo4[ which( lpo4[7]=="28HPP;28SLP" | lpo4[7]=="28HPP;28SLP;28SLPHV"), ]
+#lpo5 <- lpo4[ which( lpo4[7]=="28SLP;28HPP" | lpo4[7]=="28SLP;28HPP;28SLPHV"), ]
+##Tech Variant - Included in PDK
+flt1 <- c("28HPP")
+flt2 <- c("28HPP;28LPQRF")
+flt3 <- c("28HPP;28LPQRF;28LPQRFQCA")
+flt4 <- c("28HPP;28LPQRFQCA")
+flt5 <- c("28LPQRF")
+flt6 <- c("28LPQRF;28LPQRFQCA")
+flt7 <- c("28LPQRFQCA")
+flt8 <- c("28LPSE")
+flt9 <- c("28LPSE;28HPP")
+flt10 <- c("28LPSE;28HPP;28LPQRF")
+flt11 <- c("28LPSE;28HPP;28LPQRF;28LPQRFQCA")
+flt12 <- c("28LPSE;28HPP;28LPQRFQCA")
+flt13 <- c("28LPSE;28LPQRF")
+flt14 <- c("28LPSE;28LPQRF;28LPQRFQCA")
+flt15 <- c("28LPSE;28LPQRFQCA")
+flt16 <- c("for lpse")
+#for 28LPSE
+#lpo5 <- lpo4[ which( lpo4[7]==flt8 | lpo4[7]==flt9 |
+#lpo4[7]==flt10 | lpo4[7]==flt11 | lpo4[7]==flt12 | lpo4[7]==flt13 |
+#lpo4[7]==flt14 | lpo4[7]==flt15 | lpo4[7]==flt16 ), ]
+##for 28LPSE v2
+###grep dataframe contain keywords
+##lpo5 <- lpo4[grep("28LPSE", lpo4$TV),]
+##for 28HPP v2
+lpo5 <- lpo4[grep("28HPP", lpo4$TV),]
+##for 28LPQRF v2
+##lpo5 <- lpo4[grep("28LPQRF", lpo4$TV),]
+##for 28LPQRFQCA v2
+##lpo5 <- lpo4[grep("28LPQRFQCA", lpo4$TV),]
 #order
 lpo6 <- lpo5[order(lpo5[3]), ]
 str(lpo6)
@@ -37,9 +68,16 @@ lpo7 <- lpo6
 #write.csv(x = lpo7, row.names = FALSE, file = paste(format(Sys.time(), "%Y%m%d_%H"), "_lpo to ch2 28slphv.csv", sep = "") )
 #for slp
 #write.csv(x = lpo7, row.names = FALSE, file = paste(format(Sys.time(), "%Y%m%d_%H"), "_lpo to ch2 28slp.csv", sep = "") )
-#for slp
+#for hpp
+#write.csv(x = lpo7, row.names = FALSE, file = paste(format(Sys.time(), "%Y%m%d_%H"), "_lpo to ch2 28hpp.csv", sep = "") )
+##for 28LPSE v2
+##write.csv(x = lpo7, row.names = FALSE, file = paste(format(Sys.time(), "%Y%m%d_%H"), "_lpo to ch2 28lpse.csv", sep = "") )
+##for 28HPP v2
 write.csv(x = lpo7, row.names = FALSE, file = paste(format(Sys.time(), "%Y%m%d_%H"), "_lpo to ch2 28hpp.csv", sep = "") )
-
+##for 28LPQRF v2
+##write.csv(x = lpo7, row.names = FALSE, file = paste(format(Sys.time(), "%Y%m%d_%H"), "_lpo to ch2 28lpqrf.csv", sep = "") )
+##for 28LPQRFQCA v2
+##write.csv(x = lpo7, row.names = FALSE, file = paste(format(Sys.time(), "%Y%m%d_%H"), "_lpo to ch2 28lpqrfqca.csv", sep = "") )
 ####################################################end
 ####################################################end
 #slphv
@@ -48,6 +86,12 @@ write.csv(x = lpo7, row.names = FALSE, file = paste(format(Sys.time(), "%Y%m%d_%
 #dm1 = read.csv("28SLP_Rev1.1_5.0_DRC02 ch2 - Copy.csv", header = TRUE)
 #hpp
 dm1 = read.csv("28HPP_Rev1.1_4.0 ch2 - Copy.csv", header = TRUE)
+#lpse
+#dm1 = read.csv("28LPSe_Rev1.0_3.0 ch2 - Copy.csv", header = TRUE)
+#lpqrf
+#dm1 = read.csv("28LPQ_RF_Rev1.0_6.0 ch2 - Copy.csv", header = TRUE)
+#lpqrfqca
+#dm1 = read.csv("28LPQRF_QCA_Rev1.1_0.0_ENG ch2 - Copy.csv", header = TRUE)
 str(dm1)
 dm1 <- cbind(dm1[2]
 , dm1$v5
@@ -62,6 +106,12 @@ str(dm2)
 #write.csv(x = dm2, row.names = FALSE, file = paste(format(Sys.time(), "%Y%m%d_%H"), "_ch2 28slp.csv", sep = "") )
 #hpp
 write.csv(x = dm2, row.names = FALSE, file = paste(format(Sys.time(), "%Y%m%d_%H"), "_ch2 28hpp.csv", sep = "") )
+#lpse
+#write.csv(x = dm2, row.names = FALSE, file = paste(format(Sys.time(), "%Y%m%d_%H"), "_ch2 28lpse.csv", sep = "") )
+#lpqrf
+#write.csv(x = dm2, row.names = FALSE, file = paste(format(Sys.time(), "%Y%m%d_%H"), "_ch2 28lpqrf.csv", sep = "") )
+#lpqrfqca
+#write.csv(x = dm2, row.names = FALSE, file = paste(format(Sys.time(), "%Y%m%d_%H"), "_ch2 28lpqrfqca.csv", sep = "") )
 ####################################################end
 ####################################################end
 ####################################################end
