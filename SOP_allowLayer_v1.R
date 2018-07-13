6bb1170990d6165c7cf7bc976d97de25d093158a
HCH1 <theone_730801@hotmail.com>
2018-07-13 16:31:30 +0800

    Update SOP_allowLayer_v1.R

--- SOP_allowLayer_v1.R
+++ SOP_allowLayer_v1.R
@@ -1,6 +1,6 @@
 #install.packages("zoo")
 #library(zoo)
-lpo = read.csv("LPO-000311.csv", header = TRUE)
+lpo = read.csv("LPO-000321 22FD.csv", header = TRUE)
 #merge column 2 6 7 17 18
 lpo2 <- cbind(lpo[2],lpo[17],lpo[18],lpo[6],lpo[7],lpo[11])
 write.csv(x = lpo2, row.names = FALSE, file = paste(format(Sys.time(), "%Y%m%d_%H"), "_lpo2.csv", sep = "") )
@@ -10,8 +10,8 @@ lpo2_22fdx <- lpo2[lpo2[6]!="", ]
 lpo2_22fdx_act <- lpo2_22fdx[lpo2_22fdx[4]=="Active", ]
 #filter Layer Category != 
 lpo2_22fdx_act_cate <- lpo2_22fdx_act[lpo2_22fdx_act[5]!="Cadence Auxiliary", ]
-lpo2_22fdx_act_cate <- lpo2_22fdx_act[lpo2_22fdx_act[5]!="Generated Mask", ]
-lpo2_22fdx_act_cate <- lpo2_22fdx_act[lpo2_22fdx_act[5]!="Unknown", ]
+lpo2_22fdx_act_cate <- lpo2_22fdx_act_cate[lpo2_22fdx_act_cate[5]!="Generated Mask", ]
+lpo2_22fdx_act_cate <- lpo2_22fdx_act_cate[lpo2_22fdx_act_cate[5]!="Unknown", ]
 write.csv(x = lpo2_22fdx_act_cate, row.names = FALSE, file = paste(format(Sys.time(), "%Y%m%d_%H"), "_lpo2_22fdx_act_cate.csv", sep = "") )
 #re-bind 3 columns
 lpo2_22fdx_act_cate_3cols <- cbind(lpo2_22fdx_act_cate[1],lpo2_22fdx_act_cate[2],lpo2_22fdx_act_cate[3])
