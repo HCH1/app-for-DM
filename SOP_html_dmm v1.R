0cc0a4689c6d2d5d90d6847ed976f14d76ffe82d
HCH1 <theone_730801@hotmail.com>
2018-07-18 21:54:57 +0800

    Update SOP_html_dmm v1.R

--- SOP_html_dmm v1.R
+++ SOP_html_dmm v1.R
@@ -2,15 +2,15 @@ in1 = readLines("Pages from 22FDX_Rev1.3_3.0_PRE01-4.htm", warn = FALSE)
 str(in1)
 head(in1)
 in2 <- in1
-#grep except
+#remove fig due to waste loading time
 in2 <- grep("<IMG", in2, value = TRUE, invert = TRUE)
 #grep certain
 #in2 <- grep("ELVTP", in2, value = TRUE, invert = FALSE)
 was1 <- "ELVTP"
-will1 <- "will"
+will1 <- " will"
 ptn1 <- paste( '<del><b><font color="red">', was1, "</font></b></del>",
 '<b><font color="blue">', will1, "</font></b>", sep="" )
-#search then replace
+#search then replace, both lower/uppercase
 in2 <- gsub(was1, ptn1, in2, ignore.case = TRUE)
 ##
 ##was1 <- "elvtp"
