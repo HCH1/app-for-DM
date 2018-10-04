bfaa4a6bc56338745d999ff3f10be676e2f8fd2a
HCH1 <theone_730801@hotmail.com>
2018-10-04 10:29:32 +0800

    Update ui_ALL_22FDX_app.R

--- ui_ALL_22FDX_app.R
+++ ui_ALL_22FDX_app.R
@@ -2,41 +2,30 @@ library(shiny)
 
 ui <- fluidPage(
   
-  titlePanel("Uploading File"),
+  titlePanel("Auto-export allow layer list app"),
   
   sidebarLayout(
     
     sidebarPanel(
       
-      fileInput("file1", "Pls input 22FDX's LPO***.csv -> ALL",
+      fileInput("file1", "(1) Pls input 22FDX's LPO***.csv -> ALL",
                 multiple = TRUE,
                 accept = c("text/csv",
                            "text/comma-separated-values,text/plain",
                            ".csv")),
-      
-      tags$hr(),
-      
-      checkboxInput("header", "Header", TRUE),
-      
-      radioButtons("sep", "Separator",
-                   choices = c(Comma = ",",
-                               Semicolon = ";",
-                               Tab = "\t"),
-                   selected = ","),
-      
-      radioButtons("quote", "Quote",
-                   choices = c(None = "",
-                               "Double Quote" = '"',
-                               "Single Quote" = "'"),
-                   selected = '"'),
-      
-      tags$hr(),
-      
-      radioButtons("disp", "Display",
-                   choices = c(Head = "head",
-                               All = "all"),
-                   selected = "head")
-      
+
+##code HTML in shiny
+##https://shiny.rstudio.com/articles/tag-glossary.html
+
+tags$a(href="https://text-compare.com/", "(2) Suggest to check text_diff"),
+## <a href="www.rstudio.com">Click here!</a>
+
+tags$br(),
+tags$br(),
+
+tags$a(href="https://drive.google.com/open?id=1MKb-9hGF7S4KKJ16Cv54CuJrdXpNd_6G", 
+"(3) Upload to google drive")
+
       #      downloadButton("downloadData", "Download")
     ),
     
@@ -48,4 +37,17 @@ ui <- fluidPage(
     
   )
 )
-#runApp()
+####################################################end
+####################################################end
+#must below command separately
+##runApp()
+####################################################end
+####################################################end
+#Deploying applications
+#http://docs.rstudio.com/shinyapps.io/getting-started.html#deploying-applications
+#rsconnect::setAccountInfo(name='hch1', token='41CC9A84435E2E7CAE08CE65D932569D', secret='mEv+SQx+ACmrHLfl1qbGFdH/cc9EQj7lP8fymw3x')
+
+##library(rsconnect)
+##deployApp()
+####################################################end
+####################################################end
