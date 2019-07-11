fa873805336ec82fe3a9f8c3fb3e5ace6324fe59
HCH1 <theone_730801@hotmail.com>
2019-07-11 16:49:19 +0800

    Update ui.R

--- shiny tabs LMS_to_ALL_DMC_LMT_v1/ui.R
+++ shiny tabs LMS_to_ALL_DMC_LMT_v1/ui.R
@@ -3,12 +3,12 @@
 #install.packages("DT")
 #install.packages("dplyr")
 #install.packages("reshape2")
-#install.packages("qdapDictionaries")
+#install.packages("qdap")
 library(shiny)
 library(DT)
 library(dplyr)
 library(reshape2)
-library(qdapDictionaries)
+library(qdap)
 #
 ui <- fluidPage(
   titlePanel("app - LMS to ALL;DMC pre-check;LMT pre-check;rev history"),
