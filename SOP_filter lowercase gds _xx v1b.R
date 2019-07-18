39b265cc1e835f81207a9214c12808858669423f
HCH1 <theone_730801@hotmail.com>
2019-07-18 09:51:15 +0800

    Add files via upload

---
+++ SOP_filter lowercase gds _xx v1b.R
@@ -0,0 +1,55 @@
+#v1b grep only e.g. _MK	|_XTOR|_TYPE|_Res|_Label|_EXCL
+library(dplyr)
+##input:
+i11 = read.csv("2 LCN-003290 130G-LP (v44).csv", header = TRUE, stringsAsFactors=FALSE)
+#grep dataframe contain keywords
+#lmt_col3p3 <- "Generic"
+#lmt_col1 <- "LM-0159"
+#lmt_col2 <- "Layer Mapping Table"
+#lmt_col4 <- "Layer"
+lpo_col1 <- "LCN-003290"
+i1 <- i11
+#i1b <- i1[grep("124;0|130;0|201;0|234;39|28;0", i1$name),]
+i1b <- i1[grep("_MK	|_XTOR|_TYPE|_Res|_Label|_EXCL", i1$Data.Layer.Name),]
+###re-order
+i1b <- i1b[ order( i1b$name
+, na.last = FALSE, decreasing = FALSE ), ] #order reverse
+write.csv(x = i1b, row.names = TRUE, 
+file = paste(format(Sys.time(), "%Y%m%d_%H")
+,"_",lpo_col1
+, "_grep _xx by R v1.csv", sep = "") )
+####################################################end
+#v1 filter many gds
+library(dplyr)
+##input:
+i11 = read.csv("20190708_10_LM-0159_vs_LCN-002803_diff by R v1.csv", header = TRUE, stringsAsFactors=FALSE)
+#grep dataframe contain keywords
+lmt_col3p3 <- "Generic"
+lmt_col1 <- "LM-0159"
+lmt_col2 <- "Layer Mapping Table"
+lmt_col4 <- "Layer"
+lpo_col1 <- "LCN-002803"
+i1 <- i11
+#i1b <- i1[grep("124;0|130;0|201;0|234;39|28;0", i1$name),]
+i1b <- i1[grep("124;0|130;0|201;0|23;39|25;0|87;0", i1$name),]
+###re-order
+i1b <- i1b[ order( i1b$name
+, na.last = FALSE, decreasing = FALSE ), ] #order reverse
+write.csv(x = i1b, row.names = TRUE, 
+file = paste(format(Sys.time(), "%Y%m%d_%H")
+,"_",lmt_col1,"_vs_",lpo_col1
+, "_filter many gds by R v1.csv", sep = "") )
+####################################################end
+#v0 dmc report then grep only lowercase
+dmc = read.csv("DMC_Result_130BCDLite_DM000064 Failing Term v1.csv", header = TRUE)
+dmc1 <- dmc
+str(dmc1)
+#https://stackoverflow.com/questions/39775133/r-grep-remove-upper-case-rows
+dmc1 <- grep("^[a-z 0-9]+$|^[A-Z]{1}[a-z 0-9]+$", dmc1$Failing.Term, value = TRUE, invert = FALSE) # patterns max 2K ea
+str(dmc1)
+write.csv(x = dmc1, row.names = FALSE, 
+file = paste(format(Sys.time(), "%Y%m%d_%H")
+,"_dmc Failing Term_only lowercase.csv", sep = "") )
+####################################################end
+####################################################end
+####################################################end
