64943e4a04ae183cc329e9577b91b177f3e91bbf
HCH1 <theone_730801@hotmail.com>
2019-09-10 11:12:11 +0800

    Update spell wrong v1.R

--- spell wrong v1.R
+++ spell wrong v1.R
@@ -3,20 +3,14 @@ library(qdap)
 ##https://cran.r-project.org/web/packages/textclean/readme/README.html#stashing-character-pre-sub
 library(textclean)
 ##input:
-i1 = read.csv("11 130BCD_Rev0.9_5.0_DRC01_internal.psv.csv", header = TRUE, stringsAsFactors=FALSE)
-##
-i1_v1 <- i1
-#str(i1_v1)
-#invert grep dataframe contain keywords
-i1_v1b <- i1_v1[grep("â|Å|Γ|Ç|ª|ê|å|╧", i1_v1$Description, invert = TRUE),]
-#str(i1_v1b)
-#bad is group of list
-bad <- hunspell(i1_v1b$Description)
-#bad2 is chr
-bad2 <- sort(unique(unlist(bad)))
-bad22 <- as.character(bad2)
+#i1 = read.csv("11 130BCD_Rev0.9_5.0_DRC01_internal.psv.csv", header = TRUE, stringsAsFactors=FALSE)
+##https://cran.r-project.org/web/packages/hunspell/vignettes/intro.html
+##hunspell
+text <- readLines("11 130BCD_Rev0.9_5.0_DRC01_internal.psv.csv", warn = FALSE)
+bad_words <- hunspell(text, format = "latex")
+bad2 <- sort(unique(unlist(bad_words)))
 #bad3 is df
-bad3 <- as.data.frame(bad22)
+bad3 <- as.data.frame(bad2)
 write.csv(x = bad3, row.names = TRUE, 
           file = paste(format(Sys.time(), "%Y%m%d_%H")
                        ,"_spell wrong v1.csv", sep = "") )
